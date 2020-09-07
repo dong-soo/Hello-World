f166e93994b98d33ae531a89df5bcffd3c57f4a3
dong-soo <68644276+dong-soo@users.noreply.github.com>
2020-09-07 23:31:59 +0900

    구구단

---
+++ src/GugudanMain1.java
@@ -0,0 +1,8 @@
+public class GugudanMain1 {
+	 public static void main(String[] args) {
+		for(int i = 2; i < 10; i++) {
+			int[] result = Gugudan1.calculate(i);
+			Gugudan1.print(result);
+		}
+	}
+}
